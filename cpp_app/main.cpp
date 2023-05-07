2cf8a234ff42af3158312c67af1a23a6293bd76c
OlexandrYaremchuk <70215770+OlexandrYaremchuk@users.noreply.github.com>
2023-05-07 13:17:06 +0300

    done

---
+++ cpp_app/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	cout<< "Name:\t'The war and the peace'\t"<<endl;
+	cout << "Avtor:\tL.N. Tolstoj  " << endl;
+	cout << "Izdatelstvo:\t Piter " << endl;
+	cout << "Pages:\t 500. " << endl;
+	return(0);
+}
+
+
